a58ce2aa4e60d09cc76ef9637e508f95b4ff4adc
ECCHAN <126219665+kuronetk123@users.noreply.github.com>
2023-09-11 20:12:39 +0700

    ShoppingCartProgram.java

--- ShoppingCartProgram.java
+++ ShoppingCartProgram.java
@@ -43,25 +43,40 @@ class ShoppingCart {
     }
 
     void addItem(Product product, int quantity) {
-        Item item = this.findItemByProductId(product.id);
+        Item item = findItemByProductId(product.id);
 
         if (item != null) {
             item.quantity += quantity;
         } else {
             Item newItem = new Item(product, quantity);
 
-            this.head.prev = newItem;
-            newItem.next = this.head;
-            this.head = newItem;
+            if (isEmpty()) {
+                this.head = newItem;
+            } else {
+                newItem.next = this.head;
+                this.head.prev = newItem;
+                this.head = newItem;
+            }
         }
     }
 
     void removeItem(Product product) {
-        Item item = this.findItemByProductId(product.id);
+        Item item = findItemByProductId(product.id);
 
-        Item prevNode = item.prev, nextNode = item.next;
-        prevNode.next = nextNode;
-        nextNode.prev = prevNode;
+        if (item != null) {
+            Item prevNode = item.prev;
+            Item nextNode = item.next;
+
+            if (prevNode != null) {
+                prevNode.next = nextNode;
+            } else {
+                this.head = nextNode;
+            }
+
+            if (nextNode != null) {
+                nextNode.prev = prevNode;
+            }
+        }
     }
 
     private Item findItemByProductId(int productId) {
@@ -69,15 +84,27 @@ class ShoppingCart {
         while (node != null) {
             if (node.product.id == productId)
                 return node;
+            node = node.next;
         }
-
         return null;
     }
-
 }
 
 public class ShoppingCartProgram {
     public static void main(String args[]) {
+        Product product1 = new Product(1, "Laptop");
+        Product product2 = new Product(2, "Phone");
 
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.addItem(product1, 2);
+        cart.addItem(product2, 1);
+
+        cart.showAllItems();
+
+        cart.removeItem(product1);
+
+        cart.showAllItems();
     }
 }
+
